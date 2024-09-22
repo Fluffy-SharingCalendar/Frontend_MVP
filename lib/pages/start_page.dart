71155e7e0443ca9d9ebf616b20426fbfa2a7ea4b
sunga <tjddk6662@naver.com>
2024-09-23 04:54:07 +0900

    feat : 시작 페이지 설정
    시작 페이지로 닉네임 설정 페이지
    버튼을 누르면 닉네임이 빈 글자인지 유효성 확인 후 캘린더 페이지로 넘어감
    
    issues : #3

---
+++ lib/pages/start_page.dart
@@ -0,0 +1,113 @@
+import 'package:fluffy_mvp/pages/calendar_page.dart';
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({
+    super.key,
+  });
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  TextEditingController textEditingController = TextEditingController();
+  bool isValid = false;
+
+  bool checkValidNickname(String nickname) {
+    return nickname.isNotEmpty;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 120.0,
+            ),
+            const Text(
+              "Fluffy",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text("추억을 공유하는 캘린더, 플러피입니다🫧"),
+            const Text("현재 MVP로 수요도 조사를 진행 중입니다."),
+            const Text("원활한 이용을 위해 PC 전체화면을 권장합니다."),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              width: 500.0,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 3.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: TextField(
+                controller: textEditingController,
+                decoration: const InputDecoration(
+                  hintText: "플러피에서 사용할 닉네임을 입력해주세요.",
+                  border: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            isValid
+                ? const Text("")
+                : const Text(
+                    "닉네임은 최소 한 글자 이상이어야 합니다.",
+                    style: TextStyle(
+                      color: Colors.indigoAccent,
+                    ),
+                  ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextButton(
+              onPressed: () {
+                if (checkValidNickname(textEditingController.text)) {
+                  isValid = true;
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CalendarPage(),
+                    ),
+                  );
+                } else {
+                  isValid = false;
+                }
+              },
+              child: const Text(
+                "이 닉네임으로 할래요 🙋🏻‍♀️",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
